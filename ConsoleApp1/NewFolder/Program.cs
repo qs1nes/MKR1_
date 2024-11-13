612893af97b3f3fa50301c48e715a73b9645047e
artem <artem@DESKTOP-G1T5O88>
2024-11-13 10:17:57 +0200

    Add project files.

---
+++ ConsoleApp1/NewFolder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var input = File.ReadAllLines("INPUT.TXT");
+        int K = int.Parse(input[0]);
+        string s = input[1];
+
+        string result = GetLexicographicallyMinimalString(s, K);
+
+        File.WriteAllText("OUTPUT.TXT", result);
+    }
+
+    public static string GetLexicographicallyMinimalString(string s, int K)
+    {
+        int n = s.Length;
+        var result = new List<char>();
+        var minHeap = new SortedSet<(char ch, int index)>();
+        int start = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            minHeap.Add((s[i], i));
+
+            if (minHeap.Count > K + 1)
+            {
+                minHeap.Remove((s[start], start));
+                start++;
+            }
+
+            if (i >= K)
+            {
+                var minChar = minHeap.Min;
+                result.Add(minChar.ch);
+                minHeap.Remove(minChar);
+            }
+        }
+
+        return new string(result.ToArray());
+    }
+}
---
+++ ConsoleApp1/NewFolder/Tests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LexicographicStringTests
+{
+    [TestClass]
+    public class LexicographicallyMinimalStringTests
+    {
+        [TestMethod]
+        public void TestExampleCase()
+        {
+            int K = 2;
+            string s = "aababac";
+            string expected = "aaaabbc";
+            string result = Program.GetLexicographicallyMinimalString(s, K);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestZeroShift()
+        {
+            int K = 0;
+            string s = "abcde";
+            string expected = "abcde";
+            string result = Program.GetLexicographicallyMinimalString(s, K);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestAllSameCharacters()
+        {
+            int K = 1;
+            string s = "aaaaa";
+            string expected = "aaaaa";
+            string result = Program.GetLexicographicallyMinimalString(s, K);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMaxShift()
+        {
+            int K = 5;
+            string s = "bacdef";
+            string expected = "abcdef";
+            string result = Program.GetLexicographicallyMinimalString(s, K);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestLargeInput()
+        {
+            int K = 100;
+            string s = new string('z', 100000);
+            string expected = new string('z', 100000);
+            string result = Program.GetLexicographicallyMinimalString(s, K);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
